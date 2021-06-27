d59c8265f40dbdada69bc403056cf0598039c184
Maxim Didenko <didenko.mn@gmail.com>
2021-06-27 19:19:03 +0300

    finished auth part based on Next JS

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+App.getInitialProps = async ({ Component, ctx }) => {
+  const client = buildClient(ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return {
+    pageProps,
+    ...data
+  };
 };
 
 export default App;
--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -2,11 +2,11 @@ import { useState } from 'react';
 import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
-const Signup = () => {
+const Signin = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { errors, doRequest } = useRequest({
-    url: '/api/users/signup',
+    url: '/api/users/signin',
     method: 'post',
     body: { email, password },
     onSuccess: () => Router.push('/')
@@ -20,7 +20,7 @@ const Signup = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <h1>Sign Up</h1>
+      <h1>Sign In</h1>
       <div className="form-group">
         <label>Email Address</label>
         <input
@@ -44,10 +44,10 @@ const Signup = () => {
         type="submit" 
         className="btn btn-primary"
       >
-        Sign Up
+        Sign In
       </button>
     </form>
   );
 };
 
-export default Signup;
+export default Signin;
